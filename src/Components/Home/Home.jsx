a6973d0e67480725ffccca3b76c64601f2fc66bd
SHADMAN KHAN <khanshadman7777@gmail.com>
2024-09-13 23:35:26 +0530

    Made the hardcode design of navbar

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+export default Home;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Navbar.css";
+import instaIcon from "../../assets/instagram.png";
+import code from "../../assets/code.png";
+
+function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="portfolio-logo-container">
+        <div className="code-logo-container">
+          <img className="portfolio-logo" src={code} alt="logo" />{" "}
+        </div>
+        <h1 className="portfolio-logo-name">SHADMAN.</h1>
+      </div>
+      <ul className="nav-items-container">
+        <li className="nav-items">HOME</li>
+        <li className="nav-items">ABOUT</li>
+        <li className="nav-items">RESUME</li>
+        <li className="nav-items">CONTACT</li>
+      </ul>
+      <div className="social-media-icons-container">
+        <div className="social-media-icons">
+          <img src={instaIcon} className="social-media-icon" alt="instagram" />
+          <img src={instaIcon} className="social-media-icon" alt="instagram" />
+          <img src={instaIcon} className="social-media-icon" alt="instagram" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
